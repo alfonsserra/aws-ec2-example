c683b4258a3e3bcc792f63bb2b679f61ef2f77d5
alfonsserra <alfonsoserra@gmail.com>
2019-04-15 20:56:30 +0200

    Add VPC service

--- src/main/java/com/systelab/aws/AWSEC2ExampleApp.java
+++ src/main/java/com/systelab/aws/AWSEC2ExampleApp.java
@@ -7,6 +7,8 @@ public class AWSEC2ExampleApp {
     public static void main(String[] args) {
         EC2Service service = new EC2Service();
         service.getInstances();
-        service.createInstance("example", AMI.AMAZON_LINUX2_AMI, InstanceType.T2_MICRO);
+      //  service.createInstance("example", AMI.AMAZON_LINUX2_AMI, InstanceType.T2_MICRO);
+        VPCService service2 = new VPCService();
+        service2.getVPCs();
     }
 }
